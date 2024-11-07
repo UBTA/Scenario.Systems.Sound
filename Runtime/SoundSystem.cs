60c450c5b1ef1d328505df308a1e6c80651bb497
Weackss <62299994+UBTA@users.noreply.github.com>
2024-11-07 19:36:22 +0300

    1.1.0

---
+++ Runtime/SoundSystem.cs
@@ -0,0 +1,32 @@
+﻿using EblanDev.ScenarioCore.GameFramework.Systems;
+
+namespace EblanDev.ScenarioCore.Systems.SoundSystemUnit
+{
+    public class SoundSystem : StaticSystemDated<SoundData>
+    {
+        private static SoundWorker sounds;
+        
+        public static SoundSettings Settings => DataStatic.Settings;
+        
+        public override void Init()
+        {
+            sounds = new SoundWorker(transform);
+        }
+
+        public static void Sound(SoundID ID)
+        {
+            if (DataStatic.Settings.Mute)
+            {
+                if (sounds.GetAll().Count != 0)
+                {
+                    sounds.Clear();
+                }
+
+                return;
+            }
+            
+            var sound = sounds.Create(DataStatic.GetSound(ID));
+            sound.Play();
+        }
+    }
+}
